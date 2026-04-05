3a30ecccf8bbd8df24fb7c8aa1a46d9871d8aec5
a67715643-crypto <a67715643@gmail.com>
2026-04-05 14:50:48 +0300

    Add files via upload

--- GadiukaAi.py
+++ GadiukaAi.py
@@ -7,21 +7,9 @@ print(colorama.Back.LIGHTWHITE_EX)
 print("""               Привіт!
           Про що поговоримо?""")
 
-input()
-if input() == 'покажи кота якого миють':
- import cv2
+input = input('Я:')
 
-cat_img_path = 'cat-underwater.png'
-
-img = cv2.imread(cat_img_path)
-
-print(cat_img_path)
-print('Ось кіт, якого миють.')
-
-cv2.imshow('Cat', img)
-cv2.waitKey()
-
-if input() == 'покажи змію':
+if input == 'покажи змію':
  import cv2
 
 snake_img_path = 'thumb-article-420x300-tyc8.jpg'
